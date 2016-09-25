a4efe9bca17b1e228bdca8e9b4736ae83bc29e16
xemmel <lacour@gmail.com>
2016-09-25 07:57:13 +0200

    Form change and Added class Person

---
+++ GridList.App/Models/Peron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridList.App.Models
+{
+  public class Peron
+  {
+    public string Name { get; set; }
+    public DateTime BirthDay { get; set; }
+    public DateTime? Death { get; set; }
+
+    public bool Alive
+    {
+      get
+      {
+        return !Death.HasValue;
+      }
+    }
+  }
+}
